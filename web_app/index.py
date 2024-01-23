d5e795b9b749d739eb7f6f00fe4ae6a35c9eaf3b
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-01-23 16:42:22 -0600

    Modify index.py to work correctly and display minimal text

--- web_app/index.py
+++ web_app/index.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__Name__)
+app = Flask(__name__, template_folder="/templates")
 
-@app.round('/')
+@app.route('/')
+def index():
     return render_template('index.html')
