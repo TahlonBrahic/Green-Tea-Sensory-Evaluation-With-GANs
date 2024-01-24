16a4051b15f390bda393652ce39ff8ecd9fc4104
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-01-23 18:11:16 -0600

    Add subdirectories for web app

---
+++ web_app/services/index.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+
+TEMPLATE_DIR = os.path.abspath('../templates')
+STATIC_DIR = os.path.abspath('../static/styles')
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
