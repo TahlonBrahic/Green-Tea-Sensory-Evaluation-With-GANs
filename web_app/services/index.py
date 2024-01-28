bd1e19eaf54018e969a0a675c471ed77f1bd0b26
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-01-27 18:01:52 -0600

    Change standard html to be boilerplate and change file paths

--- web_app/services/index.py
+++ web_app/services/index.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 import os
 
-TEMPLATE_DIR = os.path.abspath('../templates')
-STATIC_DIR = os.path.abspath('../static/styles')
+TEMPLATE_DIR = os.path.abspath('/templates')
+STATIC_DIR = os.path.abspath('/static/styles')
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
