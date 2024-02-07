ecc0921ea6f93e778d77fc9f7b2c62cec1d5e53b
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-02-06 19:13:43 -0600

    Fix template and static directories

--- web_app/services/app.py
+++ web_app/services/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 import os
 
-TEMPLATE_DIR = os.path.abspath('../web_app/templates')
-STATIC_DIR = os.path.abspath('../web_app/static/styles')
+TEMPLATE_DIR = os.path.abspath('web_app/templates')
+STATIC_DIR = os.path.abspath('web_app/static/styles')
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
+print(TEMPLATE_DIR)
 
 @app.route('/')
 def index():
