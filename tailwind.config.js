744211c00615adab843962f793ef944ffed49543
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-02-10 00:10:30 -0600

    Fully implement typewriter effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./web_app/templates]",
-  theme: {
+  ttheme: {
     extend: {
       animation: {
-        typewriter: "typewriter 2s steps(11) forwards"
+        typewriter: 'typewriter 2s steps(11) forwards',
+        caret: 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
       },
       keyframes: {
         typewriter: {
           to: {
-            left: "100%"
-          }
-        }
-      }
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
     },
   },
   plugins: [],
