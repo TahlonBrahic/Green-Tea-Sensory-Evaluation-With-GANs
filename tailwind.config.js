4ee3cb3692e022ec03b8d194f2dab52276c6c497
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-02-10 00:32:14 -0600

    Adjust css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./web_app/templates]",
-  ttheme: {
+  content: ["./web_app/templates/**/*.html"],
+  theme: {
     extend: {
       animation: {
         typewriter: 'typewriter 2s steps(11) forwards',
@@ -9,29 +9,16 @@ module.exports = {
       },
       keyframes: {
         typewriter: {
-          to: {
-            left: '100%',
-          },
+          '0%': { width: 0 },
+          '100%': { width: '100%' },
         },
         blink: {
-          '0%': {
-            opacity: '0',
-          },
-          '0.1%': {
-            opacity: '1',
-          },
-          '50%': {
-            opacity: '1',
-          },
-          '50.1%': {
-            opacity: '0',
-          },
-          '100%': {
-            opacity: '0',
-          },
+          '0%': { opacity: 0 },
+          '50%': { opacity: 1 },
+          '100%': { opacity: 0 },
         },
       },
     },
   },
   plugins: [],
-}
+};
