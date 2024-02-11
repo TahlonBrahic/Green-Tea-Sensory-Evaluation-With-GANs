1403da1920db1c5c9b5c6fd3b5df41b1982366a4
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-02-11 10:56:01 -0600

    Reset tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,8 @@
 // tailwind.config.js
 module.exports = {
-  content: ["./web_app/templates/**/*.html"],
+  content: ["./web_app/templates/*.html"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      extend: {},
     },
-    colors: {
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '1': '8px',
-        '2': '12px',
-        '3': '16px',
-        '4': '24px',
-        '5': '32px',
-        '6': '48px',
-      }
-    },
-  },
   plugins: [],
-}
+};
